e764982dc0af417e83e83a589a930186df41e278
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-02-05 22:29:46 +0200

    Add documentation to ListUtils

--- Runtime/Code/Utilities/ListUtils.cs
+++ Runtime/Code/Utilities/ListUtils.cs
@@ -3,6 +3,9 @@ using System.Linq;
 
 namespace UnityCommons {
 	public static class ListUtils {
+		/// <summary>
+		/// Combines <paramref name="lists"/> into a single list
+		/// </summary>
 		public static List<T> Combine<T>(params List<T>[] lists) {
 			var combined = new List<T>();
 			foreach (var list in lists) {
@@ -11,11 +14,17 @@ namespace UnityCommons {
 
 			return combined;
 		}
-        
+		
+		/// <summary>
+		/// Combines <paramref name="lists"/> into a single list
+		/// </summary>
 		public static List<T> Combine<T>(IEnumerable<IEnumerable<T>> lists) {
 			return lists.SelectMany(list => list).ToList();
 		}
 		
+		/// <summary>
+		/// Combines <paramref name="lists"/> into a single list
+		/// </summary>
 		public static IEnumerable<T> Combine<T, TList>(IEnumerable<TList> lists) where TList : IEnumerable<T> {
 			return lists.SelectMany(list => list);
 		}
