fe08cf5c3117b6711cf0b02bedd483cc84e39268
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-02-13 14:05:29 +0200

    Make UpdateUtility monobehaviour private & unaccessible

--- Runtime/Code/Utilities/UpdateUtility.cs
+++ Runtime/Code/Utilities/UpdateUtility.cs
@@ -1,51 +1,67 @@
 using System;
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace UnityCommons {
-    public class UpdateUtility : MonoSingleton<UpdateUtility> {
-        private readonly List<Function> functions = new List<Function>();
-
-        private void Update() {
-            foreach (var function in functions) {
-                function.action();
-            }
-        }
-
-        private void RemoveAction(Function function) {
-            functions.Remove(function);
-        }
-
+    public static class UpdateUtility {
         /// <summary>
         /// Adds <paramref name="action"/> to the updater to be run every frame.
         /// </summary>
         /// <returns>An IDisposable which can be used to remove <paramref name="action"/> from updating by calling .Dispose() on it</returns>
         public static IDisposable AddUpdateAction(Action action) {
             var function = new Function(action);
-            Instance.functions.Add(function);
-            return new ActionCanceler(Instance, function);
+            UpdateUtilityUpdater.Instance.Functions.Add(function);
+            return new ActionCanceler(UpdateUtilityUpdater.Instance, function);
         }
 
+        #region Private Classes
+
+        private class UpdateUtilityUpdater : MonoSingleton<UpdateUtilityUpdater> {
+            public readonly List<Function> Functions = new List<Function>();
+
+            protected override void OnAwake() {
+                gameObject.hideFlags = HideFlags.HideAndDontSave;
+            }
+
+            private void Update() {
+                foreach (var function in Functions) {
+                    function.action();
+                }
+            }
+
+            public void RemoveAction(Function function) {
+                Functions.Remove(function);
+            }
+        }
 
         private class ActionCanceler : IDisposable {
-            private readonly UpdateUtility owner;
+            private readonly UpdateUtilityUpdater owner;
             private readonly Function function;
+            private bool disposed;
 
-            public ActionCanceler(UpdateUtility owner, Function function) {
+            public ActionCanceler(UpdateUtilityUpdater owner, Function function) {
                 this.owner = owner;
                 this.function = function;
+                disposed = false;
             }
 
             public void Dispose() {
-                owner.RemoveAction(function);
+                if (!disposed) {
+                    owner.RemoveAction(function);
+                    disposed = true;
+                }
             }
         }
 
         private class Function {
+            // ReSharper disable once InconsistentNaming
             internal readonly Action action;
 
             public Function(Action action) {
                 this.action = action;
             }
         }
+
+        #endregion
     }
 }
