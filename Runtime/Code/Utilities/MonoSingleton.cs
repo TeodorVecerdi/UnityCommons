84df2f629d00408dc65289fd181c5931e40a414a
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-02-13 11:32:14 +0200

    Make Awake not overridable and provide virtual OnAwake on MonoSingleton

--- Runtime/Code/Utilities/MonoSingleton.cs
+++ Runtime/Code/Utilities/MonoSingleton.cs
@@ -11,17 +11,21 @@ namespace UnityCommons {
 				// Find first object of type T. Other instances are destroyed when Awake is called on them.
 				if (instance == null) instance = FindObjectOfType<T>(true);
 				if (instance != null) return instance;
-				
+
 				// Create an object if cannot find an already existing one.
 				return instance = new UnityEngine.GameObject($"MonoSingleton<{type.Name}>", typeof(T)).GetComponent<T>();
 			}
 		}
 		
-		protected virtual void Awake() {
+		private void Awake() {
 			if (Instance != null && Instance != this) {
 				UnityEngine.Debug.LogError($"Cannot have multiple instances of {type.Name}. Destroying excess instances.");
 				Destroy(this);
 			}
+			
+			OnAwake();
 		}
+		
+		protected virtual void OnAwake() {}
 	}
 }
