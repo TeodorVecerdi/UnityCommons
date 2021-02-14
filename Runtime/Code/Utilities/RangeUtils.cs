ec132e3a42549b94642983cab2bb1bb0a2f16774
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-02-14 23:46:00 +0200

    Add (int, int) tuple range checks and remove float range checks

--- Runtime/Code/Utilities/RangeUtils.cs
+++ Runtime/Code/Utilities/RangeUtils.cs
@@ -26,7 +26,7 @@ namespace UnityCommons {
         public static bool RangeCheck(int value, int maxValue) {
             return RangeCheck(value, 0, maxValue);
         }
-
+        
         /// <summary>
         ///     Returns <value>true</value>
         ///     if <paramref name="value" /> is between <value>0</value> (inclusive) and <paramref name="maxValue" /> (inclusive),
@@ -35,22 +35,41 @@ namespace UnityCommons {
         public static bool RangeCheckInclusive(int value, int maxValue) {
             return RangeCheckInclusive(value, 0, maxValue);
         }
+
+        /// <summary>
+        ///     Returns <value>true</value>
+        ///     if <paramref name="value" />{x,y} is between <paramref name="minValue" />{x,y} (inclusive) and <paramref name="maxValue" />{x,y} (exclusive),
+        ///     and <value>false</value> otherwise.
+        /// </summary>
+        public static bool RangeCheck((int a, int b) value, (int a, int b) minValue, (int a, int b) maxValue) {
+            return RangeCheck(value.a, minValue.a, maxValue.a) && RangeCheck(value.b, minValue.b, maxValue.b);
+        }
+
+        /// <summary>
+        ///     Returns <value>true</value>
+        ///     if <paramref name="value" />{x,y} is between <paramref name="minValue" />{x,y} (inclusive) and <paramref name="maxValue" />{x,y} (inclusive),
+        ///     and <value>false</value> otherwise.
+        /// </summary>
+        public static bool RangeCheckInclusive((int a, int b) value, (int a, int b) minValue, (int a, int b) maxValue) {
+            return RangeCheckInclusive(value.a, minValue.a, maxValue.a) && RangeCheckInclusive(value.b, minValue.b, maxValue.b);
+        }
+
+        /// <summary>
+        ///     Returns <value>true</value>
+        ///     if <paramref name="value" />{x,y} is between <value>0</value>{x,y} (inclusive) and <paramref name="maxValue" />{x,y} (exclusive),
+        ///     and <value>false</value> otherwise.
+        /// </summary>
+        public static bool RangeCheck((int a, int b) value, (int a, int b) maxValue) {
+            return RangeCheck(value.a, 0, maxValue.a) && RangeCheck(value.b, 0, maxValue.b);
+        }
         
         /// <summary>
         ///     Returns <value>true</value>
-        ///     if <paramref name="value" /> is between <paramref name="minValue" /> (inclusive) and <paramref name="maxValue" /> (exclusive),
+        ///     if <paramref name="value" />{x,y} is between <value>0</value>{x,y} (inclusive) and <paramref name="maxValue" />{x,y} (inclusive),
         ///     and <value>false</value> otherwise.
         /// </summary>
-        public static bool RangeCheck(float value, float minValue, float maxValue) {
-            return value >= minValue && value < maxValue;
-        }
-        /// <summary>
-        ///     Returns <value>true</value>
-        ///     if <paramref name="value" /> is between <value>0</value> (inclusive) and <paramref name="maxValue" /> (exclusive),
-        ///     and <value>false</value> otherwise.
-        /// </summary>
-        public static bool RangeCheck(float value, float maxValue) {
-            return RangeCheck(value, 0, maxValue);
+        public static bool RangeCheckInclusive((int a, int b) value, (int a, int b) maxValue) {
+            return RangeCheckInclusive(value.a, 0, maxValue.a) && RangeCheckInclusive(value.b, 0, maxValue.b);
         }
     }
 }
