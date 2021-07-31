0dfeada11a62519745208fa4536b79dbcafd25e0
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-07-31 13:46:38 +0300

    Add Logging extensions with context param

--- Runtime/Code/Extensions/ObjectExtensions.cs
+++ Runtime/Code/Extensions/ObjectExtensions.cs
@@ -1,15 +1,29 @@
+using UnityEngine;
+
 namespace UnityCommons {
     public static partial class Extensions {
         public static void Log(this object value) {
-            UnityEngine.Debug.Log(value);
+            Debug.Log(value);
         }
 
         public static void LogWarning(this object value) {
-            UnityEngine.Debug.LogWarning(value);
+            Debug.LogWarning(value);
         }
 
         public static void LogError(this object value) {
-            UnityEngine.Debug.LogError(value);
+            Debug.LogError(value);
+        }
+        
+        public static void Log(this object value, Object context) {
+            Debug.Log(value, context);
+        }
+
+        public static void LogWarning(this object value, Object context) {
+            Debug.LogWarning(value, context);
+        }
+
+        public static void LogError(this object value, Object context) {
+            Debug.LogError(value, context);
         }
     }
 }
