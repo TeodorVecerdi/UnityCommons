556e9832cb593af6afd275f1e34dc137edb1b852
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-12-29 19:42:35 +0200

    Add Singletons

---
+++ Runtime/Code/Singleton/Singleton.Create.cs
@@ -0,0 +1,15 @@
+﻿namespace UnityCommons.Runtime {
+    public static partial class Singleton {
+        public abstract class Create<T> where T : Create<T> {
+            private static T instance;
+            public static T Instance => instance ??= MakeInstance();
+            public static bool IsInitialized => instance != null;
+
+            protected abstract T CreateInstance();
+            
+            private static T MakeInstance() {
+                return ((T)System.Runtime.Serialization.FormatterServices.GetUninitializedObject(typeof(T))).CreateInstance();
+            }
+        }
+    }
+}
---
+++ Runtime/Code/Singleton/Singleton.New.cs
@@ -0,0 +1,9 @@
+﻿namespace UnityCommons.Runtime {
+    public static partial class Singleton {
+        public abstract class New<T> where T : New<T>, new() {
+            private static T instance;
+            public static T Instance => instance ??= new T();
+            public static bool IsInitialized => instance != null;
+        }
+    }
+}
