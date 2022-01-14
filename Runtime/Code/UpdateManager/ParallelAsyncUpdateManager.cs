b9d0a29b012f7807da260f3a9dc543549e92f12b
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2022-01-14 15:45:52 +0200

    Fix Async Update Managers not showing up in Unity as components

---
+++ Runtime/Code/UpdateManager/ParallelAsyncUpdateManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace UnityCommons {
+    public sealed class ParallelAsyncUpdateManager : MonoSingleton<ParallelAsyncUpdateManager> {
+        private readonly AsyncUpdateEvent onUpdate = new AsyncUpdateEvent();
+        private readonly AsyncUpdateEvent onLateUpdate = new AsyncUpdateEvent();
+        private readonly AsyncUpdateEvent onFixedUpdate = new AsyncUpdateEvent();
+
+        public AsyncUpdateEvent OnUpdate {
+            get => onUpdate;
+            set {
+                if (value != onUpdate) throw new InvalidOperationException("Cannot change OnUpdate event");
+            }
+        }
+
+        public AsyncUpdateEvent OnLateUpdate {
+            get => onLateUpdate;
+            set {
+                if (value != onLateUpdate) throw new InvalidOperationException("Cannot change OnLateUpdate event");
+            }
+        }
+
+        public AsyncUpdateEvent OnFixedUpdate {
+            get => onFixedUpdate;
+            set {
+                if (value != onFixedUpdate) throw new InvalidOperationException("Cannot change OnFixedUpdate event");
+            }
+        }
+
+        private async void Update() {
+            await OnUpdate.InvokeParallel();
+        }
+
+        private async void LateUpdate() {
+            await OnLateUpdate.InvokeParallel();
+        }
+
+        private async void FixedUpdate() {
+            await OnFixedUpdate.InvokeParallel();
+        }
+    }
+}
